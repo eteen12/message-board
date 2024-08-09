441cf62de33e836afe60de9d803bf0e8bdd682e6
eteen <eteensmash@gmail.com>
2024-08-08 18:47:23 -0700

    server up and running

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const blogRoutes = require('./routes/blogRoutes');
 
 require('dotenv').config();
 
 const app = express();
 
 const dbURI = `mongodb+srv://user:${process.env.DB_PASSWORD}@messages.lborio0.mongodb.net/`;
+
+mongoose.connect(dbURI)
+.then(()=>{
+    const port = 3000;
+
+    app.listen(port);
+
+    console.log(`listening on port ${port}`);
+});
