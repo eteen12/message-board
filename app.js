e4d48694d7f8416cfecb446640bd96cce09fddf4
eteen <eteensmash@gmail.com>
2024-08-08 18:43:15 -0700

    added db and linked server

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const blogRoutes = require('./routes/blogRoutes')
+const blogRoutes = require('./routes/blogRoutes');
+
+require('dotenv').config();
 
 const app = express();
+
+const dbURI = `mongodb+srv://user:${process.env.DB_PASSWORD}@messages.lborio0.mongodb.net/`;
