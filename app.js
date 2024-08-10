17c0241817a1ab97b5a3d63d5ed1dad691b1e746
eteen <eteensmash@gmail.com>
2024-08-09 17:45:38 -0700

    trying random cahnges

--- app.js
+++ app.js
@@ -7,27 +7,24 @@ require('dotenv').config();
 
 const app = express();
 
-const dbURI = `mongodb+srv://user:${process.env.DB_PASSWORD}@messages.c2tzc.mongodb.net/`;
+const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@messages.c2tzc.mongodb.net/yourdbname?retryWrites=true&w=majority`;
 
-
-
-
-mongoose.connect(dbURI)
-.then(()=>{
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
     const port = process.env.PORT || 3000;
     app.listen(port, () => {
-        console.log(`Server listening on port ${port}`);
-    })
-})
-.catch((err)=>{
+      console.log(`Server listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
     console.error('Database connection error:', err);
-})
+  });
 
 app.set('view engine', 'ejs');
 
-//middlewear
+// Middleware
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.use(messageRoute);
